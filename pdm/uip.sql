aee7fb2ccbe1f511ee93b2551a563405f9de639b
chenmm <zjgsuchen@163.com>
2014-10-14 23:07:37 +0800

    Update SQL script.

--- pdm/uip.sql
+++ pdm/uip.sql
@@ -1,6 +1,6 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     10/14/2014 17:31:06                          */
+/* Created on:     2014/10/14 21:48:11                          */
 /*==============================================================*/
 
 
@@ -11,6 +11,8 @@ drop function if exists setval;
 drop function if exists currval;
 
 drop table if exists sequence;
+
+drop table if exists uip_user;
 
 /*==============================================================*/
 /* Table: sequence                                              */
@@ -23,6 +25,20 @@ create table sequence
    primary key (seq_name)
 );
 
+/*==============================================================*/
+/* Table: uip_user                                              */
+/*==============================================================*/
+create table uip_user
+(
+   user_id              bigint not null,
+   user_name            varchar(100) not null,
+   password             varchar(100) not null,
+   email                varchar(100) not null,
+   status               char(1) not null,
+   validate_code        varchar(100) not null,
+   register_time        datetime not null,
+   primary key (user_id)
+);
 
 create function currval (v_seq_name VARCHAR(50)) 
 RETURNS BIGINT(20)
@@ -54,4 +70,8 @@ begin
 	WHERE seq_name = v_seq_name;
 	RETURN currval(v_seq_name);
 end;
+
+INSERT INTO sequence VALUES ('UIP_SEQ', 10000000, 1);
+INSERT INTO uip_user VALUES (nextval('UIP_SEQ'), 'AI-UIP', PASSWORD('AI-UIP'), 'ai-uip@asiainfo.com', '1', 'AI-UIP', now());
+COMMIT;
 
