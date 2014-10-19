72215f3853890452dbb0b322de8c45bea9b9bc77
chenmm <zjgsuchen@163.com>
2014-10-19 13:04:23 +0800

    Format uip.sql

--- pdm/uip.sql
+++ pdm/uip.sql
@@ -4,112 +4,115 @@
 /*==============================================================*/
 
 
-drop function if exists nextval;
+DROP FUNCTION IF EXISTS nextval;
 
-drop function if exists setval;
+DROP FUNCTION IF EXISTS setval;
 
-drop function if exists currval;
+DROP FUNCTION IF EXISTS currval;
 
-drop table if exists sequence;
+DROP TABLE IF EXISTS sequence;
 
-drop table if exists uip_user;
+DROP TABLE IF EXISTS uip_user;
 
-drop table if exists uip_param;
+DROP TABLE IF EXISTS uip_param;
 
-drop table if exists uip_static_data;
+DROP TABLE IF EXISTS uip_static_data;
 
 /*==============================================================*/
 /* Table: sequence                                              */
 /*==============================================================*/
-create table sequence
+CREATE TABLE sequence
 (
-   seq_name             varchar(50) not null,
-   current_val          bigint,
-   increment_val        bigint,
-   primary key (seq_name)
+  seq_name      VARCHAR(50) NOT NULL,
+  current_val   BIGINT,
+  increment_val BIGINT,
+  PRIMARY KEY (seq_name)
 );
 
 /*==============================================================*/
 /* Table: uip_user                                              */
 /*==============================================================*/
-create table uip_user
+CREATE TABLE uip_user
 (
-   user_id              bigint not null,
-   user_name            varchar(100) not null,
-   password             varchar(100) not null,
-   email                varchar(100) not null,
-   status               char(1) not null,
-   validate_code        varchar(100) not null,
-   register_time        datetime not null,
-   primary key (user_id)
+  user_id       BIGINT       NOT NULL,
+  user_name     VARCHAR(100) NOT NULL,
+  password      VARCHAR(100) NOT NULL,
+  email         VARCHAR(100) NOT NULL,
+  status        CHAR(1)      NOT NULL,
+  validate_code VARCHAR(100) NOT NULL,
+  register_time DATETIME     NOT NULL,
+  PRIMARY KEY (user_id)
 );
 
 /*==============================================================*/
 /* Table: uip_static_data                                       */
 /*==============================================================*/
-create table uip_static_data
+CREATE TABLE uip_static_data
 (
-  data_id              bigint not null,
-  data_type            varchar(255),
-  data_value           varchar(255),
-  data_name            varchar(255),
-  date_desc            varchar(1000),
-  sort                 int,
-  status               char(1),
-  primary key (data_id)
+  data_id    BIGINT NOT NULL,
+  data_type  VARCHAR(255),
+  data_value VARCHAR(255),
+  data_name  VARCHAR(255),
+  date_desc  VARCHAR(1000),
+  sort       INT,
+  status     CHAR(1),
+  PRIMARY KEY (data_id)
 );
 
 /*==============================================================*/
 /* Table: uip_param                                             */
 /*==============================================================*/
-create table uip_param
+CREATE TABLE uip_param
 (
-  seq                  bigint not null,
-  param_name           varchar(100),
-  param_code           varchar(100),
-  param_value          varchar(100),
-  is_null              char(1),
-  sort                 int,
-  param_type           char(1),
-  remark               varchar(1000),
-  parent_seq           bigint,
-  primary key (seq)
+  seq         BIGINT NOT NULL,
+  param_name  VARCHAR(100),
+  param_code  VARCHAR(100),
+  param_value VARCHAR(100),
+  is_null     CHAR(1),
+  sort        INT,
+  param_type  CHAR(1),
+  remark      VARCHAR(1000),
+  parent_seq  BIGINT,
+  PRIMARY KEY (seq)
 );
 
-create function currval (v_seq_name VARCHAR(50)) 
-RETURNS BIGINT(20)
-begin
+CREATE FUNCTION currval(v_seq_name VARCHAR(50))
+  RETURNS BIGINT(20)
+  BEGIN
     DECLARE value BIGINT;
     SET value = 0;
-    SELECT current_val INTO value
+    SELECT
+      current_val
+    INTO value
     FROM sequence
     WHERE seq_name = v_seq_name;
-RETURN value;
-end;
+    RETURN value;
+  END;
 
 
-create function nextval (v_seq_name VARCHAR(50)) 
-RETURNS BIGINT(20)
-begin
-    UPDATE sequence 
-	SET current_val = current_val + increment_val 
-	WHERE seq_name = v_seq_name;
-	RETURN currval(v_seq_name);
-end;
-
-
-create function setval (v_seq_name VARCHAR(50), v_new_val BIGINT) 
-RETURNS BIGINT(20)
-begin
+CREATE FUNCTION nextval(v_seq_name VARCHAR(50))
+  RETURNS BIGINT(20)
+  BEGIN
     UPDATE sequence
-	SET current_val = v_new_val
-	WHERE seq_name = v_seq_name;
-	RETURN currval(v_seq_name);
-end;
+    SET current_val = current_val + increment_val
+    WHERE seq_name = v_seq_name;
+    RETURN currval(v_seq_name);
+  END;
+
+
+CREATE FUNCTION setval(v_seq_name VARCHAR(50), v_new_val BIGINT)
+  RETURNS BIGINT(20)
+  BEGIN
+    UPDATE sequence
+    SET current_val = v_new_val
+    WHERE seq_name = v_seq_name;
+    RETURN currval(v_seq_name);
+  END;
 
 INSERT INTO sequence VALUES ('UIP_SEQ', 10000000, 1);
 
-INSERT INTO uip_user VALUES (nextval('UIP_SEQ'), 'AI-UIP', PASSWORD('AI-UIP'), 'ai-uip@asiainfo.com', '1', 'AI-UIP', now());
+INSERT INTO uip_user
+VALUES (nextval('UIP_SEQ'), 'AI-UIP', PASSWORD('AI-UIP'), 'ai-uip@asiainfo.com', '1', 'AI-UIP', now());
 
 INSERT INTO uip_param VALUES (nextval('UIP_SEQ'), '根节点', 'ROOT', 'ROOT', '0', 1, '0', 'ROOT', -1);
 
