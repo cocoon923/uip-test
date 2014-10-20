b31dd9c3a4c5ccdabf4b5f27aa1630856a1c91f9
arenagod <cocoon923@vip.qq.com>
2014-10-20 08:30:20 +0800

    ready to develop

--- src/main/java/com/ailife/uip/test/db/util/StaticDataUtil.java
+++ src/main/java/com/ailife/uip/test/db/util/StaticDataUtil.java
@@ -1,7 +1,28 @@
 package com.ailife.uip.test.db.util;
 
+import com.ailife.uip.test.db.dao.IStaticDataDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
 /**
  * @author chenmm
  */
 public class StaticDataUtil {
+
+	@Autowired
+	private static IStaticDataDAO staticDataDAO;
+
+	public static Map<String, String> getStaticData(String dataType) {
+		return staticDataDAO.getStaticData(dataType);
+	}
+
+	public static String getStaticDataValue(String dataType, String dataName) {
+		return staticDataDAO.getStaticDataValue(dataType, dataName);
+	}
+
+	public enum DATATYPE{
+		PUBLIC_PARAM
+	}
+
 }
