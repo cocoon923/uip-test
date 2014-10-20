0a7c915b05c653d6e98a018736cb2110784565f7
chenmm <zjgsuchen@163.com>
2014-10-20 10:23:04 +0800

    Supply Symbol to define common symbols.

---
+++ src/main/java/com/ailife/uip/test/util/Symbol.java
@@ -0,0 +1,40 @@
+package com.ailife.uip.test.util;
+
+/**
+ * Created by chenmm on 10/20/2014.
+ */
+public class Symbol {
+
+	public final static String SEPERATOR = "|";
+
+	public final static String COMMA = ",";
+
+	public final static String PERIOD = ".";
+
+	public final static String COLON = ":";
+
+	public final static String SEMICOLON = ";";
+
+	public final static String UNDERLINE = "_";
+
+	public final static String POUND = "#";
+
+	public final static String AMPERSAND = "&";
+
+	public final static String BLANK = " ";
+
+	public final static String SLASH = "/";
+
+	public final static String COMMENT = "//";
+
+	public final static String CRLF = "\r\n";
+
+	public final static String PLUS = "+";
+
+	public final static String MINUS = "-";
+
+	public final static String SPACE = " ";
+
+	public final static String QUESTION = "?";
+
+}
