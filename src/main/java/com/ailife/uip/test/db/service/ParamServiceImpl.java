c7aba632ddd49afca5774ad102cf8e42d2083b88
arenagod <cocoon923@vip.qq.com>
2014-10-20 08:21:47 +0800

    Add ParamService and it's implement

---
+++ src/main/java/com/ailife/uip/test/db/service/ParamServiceImpl.java
@@ -0,0 +1,35 @@
+package com.ailife.uip.test.db.service;
+
+import com.ailife.uip.test.db.dao.IParamDAO;
+import com.ailife.uip.test.db.dao.IStaticDataDAO;
+import com.ailife.uip.test.util.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author chenmm
+ */
+@Service
+public class ParamServiceImpl implements IParamService {
+
+	@Autowired
+	private IParamDAO paramDAO;
+
+	@Autowired
+	private IStaticDataDAO staticDataDAO;
+
+	@Override
+	public IParamDAO getIParamDAO() {
+		return paramDAO;
+	}
+
+	@Override
+	public boolean isPublicParamInitial(String paramType) {
+		boolean isInitial = false;
+		if (StringUtils.isNotNullorEmpty(paramType)) {
+			isInitial = paramDAO.findParamCountByParentSeq(Long.parseLong(paramType)) > 0;
+		}
+		return isInitial;
+	}
+
+}
