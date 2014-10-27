6a08db08a283fac87038a5a9f2a68d7c001fd2a9
chenmm <zjgsuchen@163.com>
2014-10-27 21:01:17 +0800

    Add IInterDAO and implement

---
+++ src/main/java/com/ailife/uip/test/db/dao/IInterDAO.java
@@ -0,0 +1,10 @@
+package com.ailife.uip.test.db.dao;
+
+/**
+ * Created by chenmm on 10/27/2014.
+ */
+public interface IInterDAO extends CRUD {
+
+
+
+}
---
+++ src/main/java/com/ailife/uip/test/db/dao/impl/InterDAOImpl.java
@@ -0,0 +1,13 @@
+package com.ailife.uip.test.db.dao.impl;
+
+import com.ailife.uip.test.db.dao.IInterDAO;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by chenmm on 10/27/2014.
+ */
+@Repository
+public class InterDAOImpl extends BaseDAO implements IInterDAO {
+
+
+}
